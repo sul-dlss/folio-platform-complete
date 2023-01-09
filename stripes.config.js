1af4bc6a089c9e432a8a3c80b4e20dee61c3ec61
Shelley Doljack <sdoljack@stanford.edu>
2023-01-09 15:51:00 -0700

    Updates configuration for folio-test.

--- stripes.config.js
+++ stripes.config.js
@@ -5,8 +5,8 @@ module.exports = {
     logPrefix: '--',
     maxUnpagedResourceCount: 2000,
     showPerms: false,
-    welcomeMessage: 'FOLIO Development Instance',
-    platformName: 'FOLIO - Dev',
+    welcomeMessage: 'FOLIO Test Instance',
+    platformName: 'FOLIO - Test',
   },
   modules: {
     '@folio/acquisition-units': {},
