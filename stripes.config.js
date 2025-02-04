9a6768c053af4fabde9714506af9280c5652902d
Joshua Greben <jgreben@stanford.edu>
2025-02-04 11:33:17 -0800

    Change platformName to CSP 8

--- stripes.config.js
+++ stripes.config.js
@@ -5,7 +5,7 @@ module.exports = {
     logPrefix: '--',
     maxUnpagedResourceCount: 2000,
     welcomeMessage: 'FOLIO Quesnelia CSP 6 - Stanford University',
-    platformName: 'FOLIO - Quesnelia CSP 6',
+    platformName: 'FOLIO - Quesnelia CSP 8',
     helpUrl: 'https://sites.google.com/stanford.edu/folio-training-central/help',   
     showPerms: false,
     preserveConsole: true,
