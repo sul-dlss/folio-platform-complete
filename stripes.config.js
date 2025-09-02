2ebcb89887a638d1f627d246448af6005e771416
Joshua Greben <jgreben@stanford.edu>
2025-09-02 10:00:16 -0700

    New image for R2-2024-csp-6-folio-test (Ramsons)

--- stripes.config.js
+++ stripes.config.js
@@ -4,6 +4,9 @@ module.exports = {
     logCategories: 'core,path,action,xhr',
     logPrefix: '--',
     maxUnpagedResourceCount: 2000,
+    welcomeMessage: 'FOLIO TEST Quesnelia CSP 8 - Stanford University',
+    platformName: 'FOLIO Test Quesnelia CSP 8',
+    helpUrl: 'https://sites.google.com/stanford.edu/folio-training-central/help',
     showPerms: false,
     preserveConsole: true,
     useSecureTokens: true,
@@ -76,11 +79,11 @@ module.exports = {
   },
   branding: {
     logo: {
-      src: './tenant-assets/opentown-libraries-logo.png',
-      alt: 'Opentown Libraries',
+      src: './tenant-assets/SUSig_2color_Stree_StnfrdOnly_Left.png',
+      alt: 'Stanford University',
     },
     favicon: {
-      src: './tenant-assets/opentown-libraries-favicon.png',
+      src: './tenant-assets/stanford-favicon.png',
     },
   }
 };
