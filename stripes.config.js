ccee7635964433c27c7b86ace50fd34ba8d1f72f
Joshua Greben <jgreben@stanford.edu>
2025-12-01 15:09:17 -0800

    R2-2024-csp-8-folio-stage

--- stripes.config.js
+++ stripes.config.js
@@ -4,6 +4,9 @@ module.exports = {
     logCategories: 'core,path,action,xhr',
     logPrefix: '--',
     maxUnpagedResourceCount: 2000,
+    welcomeMessage: 'FOLIO STAGE Ramsons CSP 8 - Stanford University',
+    platformName: 'FOLIO Stage - Ramsons CSP 8',
+    helpUrl: 'https://sites.google.com/stanford.edu/folio-training-central/help',
     showPerms: false,
     preserveConsole: true,
     useSecureTokens: true,
@@ -76,8 +79,8 @@ module.exports = {
   },
   branding: {
     logo: {
-      src: './tenant-assets/opentown-libraries-logo.png',
-      alt: 'Opentown Libraries',
+      src: './tenant-assets/SUSig_2color_Stree_StnfrdOnly_Left.png',
+      alt: 'Stanford University',
     },
     favicon: {
       src: './tenant-assets/opentown-libraries-favicon.png',
