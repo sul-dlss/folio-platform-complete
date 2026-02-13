a18fcd8cd099f3f43913333479580918e0a84101
Shelley Doljack <sdoljack@stanford.edu>
2026-02-13 13:22:18 -0700

    Adds logo.png and location for customizing keycloak.

--- stripes.config.js
+++ stripes.config.js
@@ -102,7 +102,7 @@ module.exports = {
   },
   branding: {
     logo: {
-      src: './tenant-assets/SUSig_2color_Stree_StnfrdOnly_Left.png',
+      src: './tenant-assets/logo.png',
       alt: 'Stanford University',
     },
     favicon: {
